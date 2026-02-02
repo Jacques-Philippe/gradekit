f2f6c16ea83f15a9265639abf9fd707f04a566b9
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2026-02-02 06:36:04 -0500

    Remove commented code

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,29 +6,6 @@
 </template>
 
 <script setup lang="ts">
-// import { watch } from "vue";
-// import { useRouter } from "vue-router";
-// import { storeToRefs } from "pinia";
-// import { useCourseStore } from "@/stores/courseStore";
-
 import CourseInput from "@/components/CourseInput.vue";
 import CourseSelector from "@/components/CourseSelector.vue";
-
-// const router = useRouter();
-// const courseStore = useCourseStore();
-
-// const { currentCourse } = storeToRefs(courseStore);
-// watch(currentCourse, (course) => {
-//   if (course) {
-//     router.push({
-//       name: "course",
-//       params: { id: course.id },
-//     });
-//   }
-// });
-
-// async function onSelectCourse(id: string) {
-//   await courseStore.selectCourse(id);
-//   router.push({ name: "course" });
-// }
 </script>
