381c1dc9155ae5fc711f04128379705bae1d3a8c
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2026-02-03 12:59:09 -0500

    Add test for new case

--- frontend/src/stores/courseStore.test.ts
+++ frontend/src/stores/courseStore.test.ts
@@ -96,5 +96,30 @@ describe("courseStore", () => {
       expect(store.error).toContain("Failed to select course");
       expect(store.loading).toBe(false);
     });
+
+    it("resets selected course on course selection failed", async () => {
+      // we successfully select a course, then we are unsuccessful
+      // we expect for the selected course to be null
+      const mockCourse: Course = {
+        id: "a",
+        name: "Math 101",
+      };
+
+      vi.spyOn(api, "getCourseById").mockResolvedValue(mockCourse);
+
+      await store.selectCourse("a");
+
+      expect(store.currentCourse).toEqual(mockCourse);
+      expect(store.error).toBe("");
+      expect(store.loading).toBe(false);
+
+      vi.spyOn(api, "getCourseById").mockRejectedValue(new Error("Not found"));
+
+      await store.selectCourse("a");
+
+      expect(store.currentCourse).toBeNull();
+      expect(store.error).toContain("Failed to select course");
+      expect(store.loading).toBe(false);
+    });
   });
 });
