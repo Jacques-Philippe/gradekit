3bb505fabaedadabd9c4385b138f50634f370333
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2026-02-14 06:54:21 -0500

    Define view elements for course students and assignments

---
+++ frontend/src/views/CourseStudentsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="course-students-view">
+    <!-- Navigation bar -->
+    <nav class="nav-bar">
+      <BaseButton @click="goBack" aria-label="Back to course view">
+        <BackIcon />
+      </BaseButton>
+    </nav>
+
+    <!-- Page content -->
+    <main>
+      <div>Course Students View</div>
+      <!-- Student creation input -->
+      <!-- Students list, with deletion element -->
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BackIcon from "@/assets/Chevron_Left_MD.svg";
+import BaseButton from "@/components/base/BaseButton.vue";
+import { useAppStore } from "@/stores/appStore";
+import { ButtonPressedStateTransition, BACK_BUTTON_NAME } from "@/types/state";
+
+const appStore = useAppStore();
+
+function goBack() {
+  appStore.transition(new ButtonPressedStateTransition(BACK_BUTTON_NAME));
+}
+</script>
