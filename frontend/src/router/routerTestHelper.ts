ef4e8dbf3d64abf50909b5521712cba491ece5ec
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2026-04-03 05:23:13 -0400

    Update routerTesthelper

--- frontend/src/router/routerTestHelper.ts
+++ frontend/src/router/routerTestHelper.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createMemoryHistory } from "vue-router";
 import { Routes } from "@/router/routes";
 
 export function makeTestRouter() {
   return createRouter({
-    history: createWebHashHistory(),
+    history: createMemoryHistory(),
     routes: [
       { path: Routes.Home, component: { template: "<div>Home</div>" } },
       { path: Routes.Login, component: { template: "<div>Login</div>" } },
