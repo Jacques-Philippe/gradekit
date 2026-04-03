33653c6eb505605af6bbdb97095081d3a7657d64
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2026-04-03 05:40:20 -0400

    Use .env file for vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,32 @@
 import { defineConfig } from "vitest/config";
+import { loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 import svgLoader from "vite-svg-loader";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/gradekit/",
-  plugins: [vue(), svgLoader()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-  },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    base: "/gradekit/",
+    plugins: [vue(), svgLoader()],
+    test: {
+      environment: "jsdom",
+      globals: true,
     },
-  },
-  server: {
-    host: true,
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_URL ?? "http://localhost:8000",
-        rewrite: (path) => path.replace(/^\/api/, ""),
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-  },
+    server: {
+      host: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL ?? "http://localhost:8000",
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
 });
